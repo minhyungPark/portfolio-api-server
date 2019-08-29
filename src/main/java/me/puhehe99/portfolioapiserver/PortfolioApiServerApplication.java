c27728276579eda14c5c83007e83f5a7fe5160ea
minhyungPark <puhehe99@gmail.com>
2019-08-29 11:35:14 +0900

    Add base project

---
+++ src/main/java/me/puhehe99/portfolioapiserver/PortfolioApiServerApplication.java
@@ -0,0 +1,13 @@
+package me.puhehe99.portfolioapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApiServerApplication.class, args);
+    }
+
+}
